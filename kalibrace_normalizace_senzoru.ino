d44f314c3fa0c020f175c41d11e45c8c81433435
Lukáš Hronek <hutulek.h@seznam.cz>
2017-03-03 18:47:08 +0100

    Kalibrování, normalizování hodnot z foto rezistoru
    Kalibrování ve funkci setMinMax(),  normalizování hodnot ve funkci
    getNormalizedValue(),
    plovoucí průměr k vyhlazení signálu je samozřejmost.

--- kalibrace_normalizace_senzoru.ino
+++ kalibrace_normalizace_senzoru.ino
@@ -1,9 +1,73 @@
+
+int _foto = A0;
+
+int maxVal = 860;
+int minVal = 524;
+
+const int width = 30;
+int hodnoty[width];
+int index = 0;
+
+int sum = 0;
+
+int getFloatAverage(int input)
+{
+  sum -= hodnoty[index];
+  hodnoty[index] = input;
+  sum += input;
+  index++;
+  if(index == width)
+  {
+    index = 0;
+  }
+
+  return sum/width;
+}
+
+void setMinMax(int caliTime)
+{
+  digitalWrite(_led, HIGH);
+  for(int i = 0; i< caliTime * 1000; i++)
+  {
+    int val = analogRead(_foto);
+    if(val>maxVal)
+      maxVal = val;
+    if(val<minVal)
+      minVal = val;
+
+      delay(1);
+  }
+  digitalWrite(_led, LOW);
+}
+
+float getNormalizedValue(int val)
+{
+  if(val>maxVal) val = maxVal;
+  if(val<minVal) val = minVal;
+
+  int citatel = val - minVal;
+  int jmenovatel = maxVal - minVal;
+
+  float out = (float)citatel / (float)jmenovatel;
+  return out * 255;
+}
+
+
 void setup() {
   // put your setup code here, to run once:
-
+  //pinMode(_led,OUTPUT);
+  Serial.begin(9600);
+  setMinMax(5);  
+  Serial.println(minVal);
+  Serial.println(maxVal);
+  for(int i = 0; i < width; i++)
+  {
+    getFloatAverage(analogRead(_foto)); 
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
+ Serial.println(getNormalizedValue(getFloatAverage(analogRead(_foto))));
 }
